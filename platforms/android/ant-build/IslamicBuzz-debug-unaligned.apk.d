72bc93f3799415cf69a36ed14325d79efec29ba4
Noor Alam <Noor.Alam@globallogic.com>
2014-09-25 20:09:45 +0530

    SlidingMenu library adde

--- platforms/android/ant-build/IslamicBuzz-debug-unaligned.apk.d
+++ platforms/android/ant-build/IslamicBuzz-debug-unaligned.apk.d
@@ -2,3 +2,4 @@
 /Users/noor.alam/Documents/Proj/CordovaProj/HHAAHHJJ/platforms/android/ant-build/IslamicBuzz.ap_ \
 /Users/noor.alam/Documents/Proj/CordovaProj/HHAAHHJJ/platforms/android/ant-build/classes.dex \
 /Users/noor.alam/Documents/Proj/CordovaProj/HHAAHHJJ/platforms/android/CordovaLib/ant-build/classes.jar \
+/Users/noor.alam/Documents/Proj/CordovaProj/HHAAHHJJ/platforms/android/libs/android-support-v4.jar \
